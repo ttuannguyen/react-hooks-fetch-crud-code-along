92d296487d466ecd1e0e1ca78f578b82ee9b2bd4
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-03-11 23:55:25 -0500

    commit tree 22661400de980ced499d1cffbc000519b10fe64c

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,51 @@
 import React from "react";
 
-function Item({ item }) {
+//Deconstruct the onUpdateItem prop
+function Item({ item, onUpdateItem, onDeleteItem }) {
+
+  function handleAddToCartClick() {
+    // add fetch request
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      //purpose: toggle property of the item since our goal is to let users add / remove items
+      //we send the object with the key being updated along w/ the new value
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedItem) => onUpdateItem(updatedItem));
+  }
+
+  function handleDeleteClick() {
+    console.log(item)
+    //add fetch request
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE",
+    })
+    .then(res => res.json())
+    .then(() => onDeleteItem(item));
+  }    
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button 
+        onClick={handleAddToCartClick} 
+        className={item.isInCart ? "remove" : "add"}
+      >
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button 
+        onClick={handleDeleteClick}
+        className="remove"
+      >
+        Delete
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,40 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  //Add useEffect hook
+  useEffect(() => {
+    fetch('http://localhost:4000/items')
+    .then(res => res.json())
+    .then(items => setItems(items))
+  }, []); //NOTE: don't place any ; after the .then()s until the end of the function scope
+
+  //new function to handle add item to page
+  function handleAddItem(newItem) {
+    //last step for adding a new item (related to post): call setState w/ new arr that has new item
+    //use spread operator so we don't mutate the arr
+    setItems([...items, newItem])
+  }
+
+  //new function to handle cart add/remove update
+  function handleUpdateItem(updatedItem) {
+    //last step for this fn (related to patch) = set state by creating a new array which contains the updated item in place of the old item
+    const updatedItems = items.map(item => {
+      if(item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
+  //new function to handle delete
+  function handleDeleteItem(deletedItem) {
+    //last step for this fn (related to delete) = set state by creating a new arr in which the deleted item has been filtered out
+    const updatedItems = items.filter(item => item.id !== deletedItem.id)
+    setItems(updatedItems)
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,14 +53,19 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item 
+            key={item.id} 
+            item={item} 
+            onUpdateItem={handleUpdateItem}
+            onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
